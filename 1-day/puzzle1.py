46d766e936d2a5de7587228e0ca3201036a007d5
Khabib Toshev <khabibtoshev@gmail.com>
2025-12-01 12:39:22 +0500

    day 1 solutions

---
+++ 1-day/puzzle1.py
@@ -0,0 +1,22 @@
+import os
+
+base = os.path.dirname(__file__)
+path = os.path.join(base, "input")
+
+s = 50
+cnt = 0
+
+with open(path, 'r') as file:
+    for line in file:
+        direction, value = line[0], int(line[1:])
+        if direction == 'L':
+            s -= value
+        elif direction == 'R':
+            s += value
+        s %= 100
+        if s == 0:
+            cnt += 1
+        
+        # print(direction, value, cnt)
+
+print(cnt)
---
+++ 1-day/puzzle2.py
@@ -0,0 +1,27 @@
+import os
+
+base = os.path.dirname(__file__)
+path = os.path.join(base, "input")
+
+s = 50
+cnt = 0
+
+with open(path, 'r') as file:
+    for line in file:
+        direction, value = line[0], int(line[1:])
+
+        cnt += value // 100
+        value %= 100
+
+        if direction == 'L':
+            if value >= s > 0:
+                cnt += 1
+            s = (s - value) % 100
+        elif direction == 'R':
+            if s + value > 99:
+                cnt += 1
+            s = (s + value) % 100
+        
+        # print(direction, value, cnt)
+
+print(cnt)
